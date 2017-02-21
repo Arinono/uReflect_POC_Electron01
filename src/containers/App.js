02287c2d15f5566b80c57219293971bbc8b6fc27
Aurelien ARINO <aurelien.arino@epitech.eu>
2017-02-21 09:35:06 +0100

    Spotify Player

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import TestWidget from '../components/TestWidget';
 import ClockContainer from './ClockContainer';
 import Weather from '../components/Weather';
+import SpotifyContainer from './SpotifyContainer';
 
 var App = React.createClass({
   render: function() {
     return (
       <div className="widgetContainer">
-        <ClockContainer />
+        <SpotifyContainer />
       </div>
     );
   }
